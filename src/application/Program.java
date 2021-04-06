0257cb029667d7ddabf5f63dcdc03cda7bbba4d4
Pedro Bibiano <pedroedubibiano@gmail.com>
2021-04-06 18:41:18 -0300

    Chess-take2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
  public static void main(String ars[]) {
 	 
-	 System.out.println("--");
+	 Position pos = new Position(3, 5);
+	 System.out.println(pos);
 	 
  }
 }
